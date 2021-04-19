1f56d021eca084912fee8580dee51a3388e20004
Lucas Caniggia <lucascaniggia5@gmail.com>
2021-04-19 18:05:56 -0300

    HTTP requests and back end working.

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
 const express = require('express');
 const path = require('path');
 const PORT = 4000;
+const cors = require('cors');
+
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 app.listen(PORT, () => console.log('Server started on port 4000'));
 
-app.put('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, 'Radium Rocket', 'Semana 9', 'index.html')) // Modelo de formato, todavía no funcional
-});
+app.put('/login', handleLogin)
 
-app.post('/register', (req, res) => {
-    res.sendFile(path.join(__dirname, '', ''));
-});
+app.post('/register', handleRegister)
 
-function handleLogin() {
+function handleLogin(req, res) {
+    console.log(req.body)
 
+    const userLogin = {
+        email: req.body.mailValue,
+        password: req.body.passValue,
+    };
+    if (userLogin.email && userLogin.password1) {
+        console.log("Ok");
+        return res.json({ result: 'User data found successfully', userLogin });
+    } else {
+        throw new Error('User data not found');
+    }
 };
 
-function handleRegister() {
+function handleRegister(req, res) {
+
+    const userRegist = {
+        name: req.body.nameRegValue,
+        email: req.body.mailRegValue,
+        password: req.body.passRegValue,
+    };
+    console.log(userRegist);
+    res.json({ Result: 'Data sent' });
 
 };
